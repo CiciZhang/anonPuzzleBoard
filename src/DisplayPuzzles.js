7341bca5fb3a2814801d0559619c314e3273d9e5
CC <cici.j.zhang@gmail.com>
2020-08-09 05:06:59 -0400

    going to bed, going to figure out the answer input issue tomorrow

--- src/DisplayPuzzles.js
+++ src/DisplayPuzzles.js
@@ -11,14 +11,48 @@ class DisplayPuzzle extends Component {
     }
 
     componentDidMount() {
-        dbRef.on('value', (snapshot) =>
-            console.log(snapshot.val()))
+        dbRef.on('value', (snapshot) => {
+            console.log(snapshot.val())
+            const dbObject = snapshot.val()
+            const newReturnedArray = []
+            for (let propertyName in dbObject) {
+                newReturnedArray.push(dbObject[propertyName])
+            }
+            console.log(newReturnedArray)
+            this.setState({
+                dbReturn: newReturnedArray,
+                riddleAnswer: ""
+            }) 
+            console.log(this.state.dbReturn, "this is my dbReturn")
+        })
+    }
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+        console.log(this.state.riddleInput, "this is the riddleInput")
+        console.log(this.state.answerInput, "this is the answerInput")
+    }
+    checkRiddleAnswer = (event) =>{
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+        console.log(this.state.riddleAnswer)
     }
     
     render() {
         return(
             <div className="displayBox">
-                <h1>If this shows up it's linked correctly</h1>
+                <h1>Anonymous Puzzle Board</h1>
+                <ul>
+                    {this.state.dbReturn.map(item => {
+                    return <form action="submit">
+                        <p>{item[0]}</p>
+                        {console.log(item), "item's this"}
+                        <input type="text" id="riddleAnswer" onChange={this.checkRiddleAnswer} name="riddleAnswer" value={this.state.riddleAnswer} placeholder="Answer" />
+                    </form>
+                    })}
+                </ul>
             </div>
            
         )
